2bbf812109ed3e7ef49ffcf7c457f0c941d492e0
xiaogao <gc1714797713@gmail.com>
2024-05-29 21:41:54 +0800

    添加路由

--- meeting_room_booking_system_frontend_user/src/index.tsx
+++ meeting_room_booking_system_frontend_user/src/index.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter, Link, Outlet } from 'react-router-dom';
+import { Login } from './page/login';
+import { Register } from './page/register';
+import { UpdatePassword } from './page/updatePassword';
+
+function Aaa() {
+  return <div>aaa</div>;
+}
+
+function Bbb(){
+  return <div>bbb</div>;
+}
+
+function Layout() {
+  return <div>
+    <div><Link to="/aaa">to aaa</Link></div>
+    <div><Link to="/bbb">to bbb</Link></div>
+    <div>
+      <Outlet/>
+    </div>
+  </div>
+}
+
+function ErrorPage() {
+  return <div>error</div>;
+}
+
+const routes = [
+  {
+    path: "/",
+    element: <div>index</div>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "update_password",
+    element: <UpdatePassword />,
+  }
+];
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RouterProvider router={router}/>);
