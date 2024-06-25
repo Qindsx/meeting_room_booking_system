5334501684b754f3c16a175b4257256b2fb7932a
Qindsx <1714797713@qq.com>
2024-06-25 22:35:57 +0800

    会议室管理模块-创建接口

--- meeting_room_booking_system_backend/src/modules/meeting-room/meeting_room.controller.ts
+++ meeting_room_booking_system_backend/src/modules/meeting-room/meeting_room.controller.ts
@@ -5,17 +5,25 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { MeetingRoom } from './entities/meeting_room.entity';
 import { Repository } from 'typeorm';
 import { generateParseIntPipe } from 'src/utils/utils';
+import { RequireLogin } from 'src/decorator/custom.decorator';
 
 @Controller('meeting-room')
 export class MeetingRoomController {
   constructor(private readonly meetingRoomService: MeetingRoomService) { }
 
   @Get('list')
+  @RequireLogin()
   async list(
     @Query('pageNo', new DefaultValuePipe(1), generateParseIntPipe('pageNo')) pageNo: number,
     @Query('pageSize', new DefaultValuePipe(2), generateParseIntPipe('pageSize')) pageSize: number,
   ){
     return await this.meetingRoomService.getList(pageNo,pageSize)
+  }
+
+  @Post('create')
+  @RequireLogin()
+  async create(@Body() meetingRoomDto:CreateMeetingRoomDto) {
+    return await this.meetingRoomService.cerate(meetingRoomDto)
   }
 
 }
--- meeting_room_booking_system_backend/src/modules/meeting-room/meeting_room.service.ts
+++ meeting_room_booking_system_backend/src/modules/meeting-room/meeting_room.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { BadRequestException, HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { CreateMeetingRoomDto } from './dto/create-meeting_room.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -9,6 +9,7 @@ export class MeetingRoomService {
   @InjectRepository(MeetingRoom)
   private repository:Repository<MeetingRoom>
 
+  // 初始化数据方法
   initData() {
     const room1 = new MeetingRoom();
     room1.name = '木星';
@@ -31,7 +32,7 @@ export class MeetingRoomService {
     this.repository.insert([room1, room2, room3])
   }
 
-  
+  // 获取列表数据
   async getList(pageNo: number, pageSize: number) {
     if(pageNo<0){
       return new HttpException('pageNo必须大于0', HttpStatus.BAD_REQUEST)
@@ -46,5 +47,16 @@ export class MeetingRoomService {
       meetingRoomList,
       totalCount
     }
+  }
+
+  // 创建方法
+  async cerate(meetingRoomDto:CreateMeetingRoomDto) {
+    let room = await this.repository.findOneBy({
+      name:meetingRoomDto.name
+    })
+    if(room) {
+      throw new BadRequestException('该会议室已存在')
+    }
+    return (await this.repository.insert(meetingRoomDto)).generatedMaps
   }
 }
